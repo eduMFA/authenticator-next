b712f891f675dcf25bfa9d58058ff920e55899d6
Lucas Hardt <lucas.hardt@fu-berlin.de>
2026-04-16 13:09:37 +0200

    feat(assets): update app and splash screen icons to the eduMFA logo

--- app.config.ts
+++ app.config.ts
@@ -28,7 +28,7 @@ const config: ExpoConfig = {
   scheme: "edumfa-push",
   assetBundlePatterns: ["**/*"],
   ios: {
-    //icon: "./assets/app-icons/edumfa.icon",
+    icon: "./assets/app-icons/edumfa.icon",
     supportsTablet: false,
     bundleIdentifier: getAppId(),
     userInterfaceStyle: "automatic",
@@ -46,7 +46,6 @@ const config: ExpoConfig = {
   android: {
     adaptiveIcon: {
       foregroundImage: "./assets/app-icons/icon-android.png",
-      backgroundImage: "./assets/app-icons/icon-android-background.png",
       monochromeImage: "./assets/app-icons/icon-monochrome-android.png",
     },
     userInterfaceStyle: "automatic",
@@ -70,6 +69,7 @@ const config: ExpoConfig = {
         resizeMode: "contain",
         backgroundColor: "#ffffff",
         dark: {
+          image: "./assets/splash-icon-dark.png",
           backgroundColor: "#000000",
         },
       },
