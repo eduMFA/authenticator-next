0d5581318569578f675813634ab1eabb66446446
Lucas Hardt <lucas.hardt@fu-berlin.de>
2026-07-01 09:21:43 +0200

    feat(dev): add option to spawn sample tokens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -223,6 +223,9 @@ export default function Tokens() {
             <Stack.Toolbar.MenuAction onPress={devMenu.clearPushRequests}>
               Clear Push Requests
             </Stack.Toolbar.MenuAction>
+            <Stack.Toolbar.MenuAction onPress={devMenu.spawnSampleTokens}>
+              Spawn Sample Tokens
+            </Stack.Toolbar.MenuAction>
             <Stack.Toolbar.MenuAction onPress={devMenu.resetOnboarding}>
               Show Onboarding
             </Stack.Toolbar.MenuAction>
